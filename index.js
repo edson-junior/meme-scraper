d213c8fbc0d8a1cc13b8a69c995191ddec4f711c
Edson Junior <soneddjr@gmail.com>
2026-02-12 10:46:21 +0100

    Remove unnecessary try/catch

--- index.js
+++ index.js
@@ -17,35 +17,31 @@ if (!fs.existsSync(memesDir)) {
  * @returns {Promise<void>}
  */
 async function scrape() {
-  try {
-    const response = await fetch(url);
+  const response = await fetch(url);
 
-    // we're not interested in proceeding if there's an error requesting the website
-    if (response.status !== 200) {
-      console.error(response.status, response.statusText);
-      throw new Error('Failed to fetch data');
+  // we're not interested in proceeding if there's an error requesting the website
+  if (response.status !== 200) {
+    console.error(response.status, response.statusText);
+    throw new Error('Failed to fetch data');
+  }
+
+  const html = await response.text();
+  const $ = cheerio.load(html);
+
+  // create an array of the 10 first images
+  $('#images > div').each((index, imageWrapper) => {
+    if (index === 10) {
+      return false;
     }
 
-    const html = await response.text();
-    const $ = cheerio.load(html);
+    const src = $(imageWrapper).find('img').attr('src');
 
-    // create an array of the 10 first images
-    $('#images > div').each((index, imageWrapper) => {
-      if (index === 10) {
-        return false;
-      }
+    images.push(src);
+  });
 
-      const src = $(imageWrapper).find('img').attr('src');
-
-      images.push(src);
-    });
-
-    // iterate over the images and download them
-    for (let index = 0; index < images.length; index++) {
-      await downloadImage(images[index], index + 1);
-    }
-  } catch (error) {
-    throw error;
+  // iterate over the images and download them
+  for (let index = 0; index < images.length; index++) {
+    await downloadImage(images[index], index + 1);
   }
 }
 
